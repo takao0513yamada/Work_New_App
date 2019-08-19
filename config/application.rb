547e67b4cbaece51b863a1a63efd53f7df7322b8
taka president <life.i.s.w---@i.softbank.jp>
2019-08-19 20:10:47 +0900

    データベース環境構築 by Docker

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,11 @@ module RaiseTechApp1
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
